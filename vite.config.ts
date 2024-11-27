8139f574cb8333c91f50d49f86188f228c859e96
= <=>
2024-11-26 21:45:53 -0500

    package fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		fs: {
-			allow: [
-				// Allow serving files from project root
-				'.',
-				// Allow serving files from node_modules
-				'../../node_modules',
-				// Specifically allow @sveltejs/kit runtime files
-				fileURLToPath(new URL('../../node_modules/@sveltejs/kit', import.meta.url)),
-				'static'
-			]
-		}
-	}
+	plugins: [sveltekit()]
 });
